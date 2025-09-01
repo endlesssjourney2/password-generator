35d4fcd59fcae86739d8d252702ba59678730970
endlesssjourney <fordast10@gmail.com>
2025-09-01 16:17:33 +0200

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { NavLink, Route, Routes } from "react-router-dom";
+import { Navigate, NavLink, Route, Routes } from "react-router-dom";
 import GeneratePage from "./pages/GeneratePage/GeneratePage";
 import SavedPage from "./pages/SavedPage/SavedPage";
 
@@ -9,16 +9,16 @@ const App = () => {
       <header className="header">Password Generator 🔐</header>
       <nav className="nav">
         <NavLink className="nav-link" to="/">
-          Home
+          Generate
         </NavLink>
         <NavLink className="nav-link" to="/saved">
           Saved
         </NavLink>
       </nav>
       <Routes>
-        <Route path="/password-generator" element={<GeneratePage />} />
         <Route path="/" element={<GeneratePage />} />
         <Route path="/saved" element={<SavedPage />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
       <footer className="footer">
         endlesssjourney© 2025 Password Generator
